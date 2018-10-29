623c3b6c9d2bf116f38273dda2c3ee376110e8ba
清风 <jiegithub@gmail.com>
2018-10-30 00:07:18 +0800

    feat: 修改rollup打包配置

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,32 @@
 'use strict'
 import path from 'path'
 import nodeResolve from 'rollup-plugin-node-resolve'
-import jsonResolve from 'rollup-plugin-json'
+import json from 'rollup-plugin-json'
+import commonjs from 'rollup-plugin-commonjs'
 
 export default {
   input: path.resolve('src/index.js'),
-  plugins: [nodeResolve(), jsonResolve()],
-  output: {
-    file: 'dist/rollup-index.js',
-    format: 'cjs',
-    banner: '// this is created from /src/index.js'
-  }
+  plugins: [nodeResolve(), commonjs(), json()],
+  output: [
+    {
+      file: 'dist/h5ds.esm.js',
+      format: 'es'
+    },
+    {
+      file: 'dist/h5ds.cmd.js',
+      format: 'cjs'
+    },
+    {
+      file: 'dist/h5ds.iife.js',
+      format: 'iife'
+    },
+    {
+      file: 'dist/h5ds.umd.js',
+      format: 'umd'
+    },
+    {
+      file: 'dist/h5ds.amd.js',
+      format: 'amd'
+    }
+  ]
 }
